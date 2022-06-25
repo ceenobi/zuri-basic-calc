08fe4e4aa225c0580ead98121d3d4e87c7a6350a
ceenobi <cobimbachu@gmail.com>
2022-06-25 16:02:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let add = (num1, num2) => {
+  let adddNumbers = num1 + num2
+  console.log(adddNumbers)
+}
+add(5, 10)
+let subtraction = (num1, num2) => {
+  let subtractNumbers = num1 - num2
+  console.log(subtractNumbers)
+}
+subtraction(7, 3)
+let multiplication = (num1, num2) => {
+  let multipliedNumbers = num1 * num2
+  console.log(multipliedNumbers)
+}
+multiplication(4, 4)
+let Division = (num1, num2) => {
+  let dividedNumbers = num1 / num2
+  console.log(dividedNumbers)
+}
+Division(10, 2)
